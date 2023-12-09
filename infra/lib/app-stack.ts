30165ef9327ab661e08ff429baa5f575abd50476
Fredrik F. Lindhagen <fredrik.forde.lindhagen@dnb.no>
2023-12-09 18:18:54 +0100

    More example output to function

--- infra/lib/app-stack.ts
+++ infra/lib/app-stack.ts
@@ -1,10 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Construct} from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import {FunctionUrlAuthType} from 'aws-cdk-lib/aws-lambda';
 
-export class InfraStack extends cdk.Stack {
+export class AppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const listLocationsFunction = new lambda.Function(this, 'list-locations-v1', {
+      functionName: `${id}-list-locations-v1`,
+      runtime: lambda.Runtime.PROVIDED_AL2023,
+      handler: 'rust.handler',
+      code: lambda.Code.fromAsset('../list-locations-v1/target/lambda/list-locations-v1/bootstrap.zip'),
+      architecture: lambda.Architecture.ARM_64,
+    })
+
+    listLocationsFunction.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+    })
 
     // The code that defines your stack goes here
 
--- list-locations-v1/src/main.rs
+++ list-locations-v1/src/main.rs
@@ -17,7 +17,7 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
         .query_string_parameters_ref()
         .and_then(|params| params.first("name"))
         .unwrap_or("world");
-
+    tracing::info!("Retrieving list of locations...");
     let locations = [
       Location {
           id: Uuid::new_v4().to_string(),
@@ -28,8 +28,30 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
           },
           toilet: Option::from(Toilet { code: 2023 }),
           wifi: Option::from(Wifi { code: None, is_open_network: true }),
-      }  
+      },
+      Location {
+          id: Uuid::new_v4().to_string(),
+          name: "Baker hansen Torshov".to_string(),
+          location: Coordinate {
+              lat: 59.9345054,
+              lon: 10.7639602,
+          },
+          toilet: Option::from(Toilet { code: 4321 }),
+          wifi: Option::from(Wifi { code: Some("Kanelbolle".to_string()), is_open_network: false }),
+      },
+      Location {
+          id: Uuid::new_v4().to_string(),
+          name: "Espresso house Majorstua".to_string(),
+          location: Coordinate {
+              lat: 59.9345054,
+              lon: 10.7639602,
+          },
+          toilet: Option::from(Toilet { code: 4578 }),
+          wifi: Option::from(Wifi { code: None, is_open_network: true }),
+      },
     ];
+    let locations_formatted = serde_json::to_string(&locations).unwrap();
+    tracing::info!({ %locations_formatted }, "Locations retrieved!");
 
     // Return something that implements IntoResponse.
     // It will be serialized to the right response event automatically by the runtime
@@ -40,6 +62,7 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt()
+        .json()
         .with_max_level(tracing::Level::INFO)
         // disable printing the name of the module in every log line.
         .with_target(false)
