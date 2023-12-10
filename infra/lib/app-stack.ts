0bd0c0b166f032bb45b64ad865a1e6703837ce8b
Fredrik F. Lindhagen <fredrik.forde.lindhagen@dnb.no>
2023-12-10 15:53:16 +0100

    Add API Gateway resource

--- infra/lib/app-stack.ts
+++ infra/lib/app-stack.ts
@@ -4,8 +4,19 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { AttributeType, BillingMode, TableEncryption } from 'aws-cdk-lib/aws-dynamodb';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { IRestApi, SecurityPolicy } from 'aws-cdk-lib/aws-apigateway';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as route53Targets from 'aws-cdk-lib/aws-route53-targets';
+import { CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager';
+import { HostedZone, IHostedZone } from "aws-cdk-lib/aws-route53";
+
+const domainName = 'toalettkoder.prod.lokalvert.tech';
 
 export class AppStack extends cdk.Stack {
+  private readonly hostedZoneId = 'Z07564492IFZPEI4UUD2C';
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -24,6 +35,14 @@ export class AppStack extends cdk.Stack {
       }
     });
 
+    const hostedZone = HostedZone.fromHostedZoneAttributes(this, 'HostedZone', {
+      hostedZoneId: this.hostedZoneId,
+      zoneName: 'prod.lokalvert.tech',
+    });
+
+    const api = this.buildRestApi(this, 'RestApi', hostedZone, domainName);
+    const v1Resource = api.root.addResource('v1');
+
     const listLocationsFunction = new lambda.Function(this, 'list-locations-v1', {
       functionName: `${id}-list-locations-v1`,
       runtime: lambda.Runtime.PROVIDED_AL2023,
@@ -37,15 +56,32 @@ export class AppStack extends cdk.Stack {
 
     locationsTable.grantReadData(listLocationsFunction);
 
-    listLocationsFunction.addFunctionUrl({
-      authType: FunctionUrlAuthType.NONE,
-    })
+    // URL /v1/locations/ is where we'll place locations specific actions
+    const locationsResource = v1Resource.addResource('locations');
+    locationsResource.addMethod('GET', new apigateway.LambdaIntegration(listLocationsFunction));
+  }
 
-    // The code that defines your stack goes here
+  private buildRestApi(scope: Construct, idPrefix: string, hostedZone: IHostedZone, domainName: string): IRestApi {
+    const certificate = new acm.Certificate(scope, 'ApiCertificate', {
+      domainName,
+      validation: CertificateValidation.fromDns(hostedZone),
+    });
+    const api = new apigateway.RestApi(scope, 'RestApi', {
+      restApiName: 'toalettkoder',
+      description: 'Provide easy availability to valuable information of locations',
+      endpointTypes: [apigateway.EndpointType.REGIONAL],
+      domainName: {
+        domainName,
+        certificate: certificate,
+        securityPolicy: SecurityPolicy.TLS_1_2,
+      },
+    });
+    new route53.ARecord(scope, 'ApiDnsRecord', {
+      recordName: domainName,
+      zone: hostedZone,
+      target: route53.RecordTarget.fromAlias(new route53Targets.ApiGateway(api)),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    return api;
   }
 }
